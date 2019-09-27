b70ffbbf64158f62c5cc6d76bb13170a175d8e7f
nicksarris <ngs5st@virginia.edu>
2019-09-27 16:38:18 -0400

    Load model before calling service (speedup ~5x)

--- service/bert_service.py
+++ service/bert_service.py
@@ -25,38 +25,7 @@ from random import shuffle
 from tqdm._tqdm_notebook import tqdm_notebook as tqdm
 from keras.preprocessing.sequence import pad_sequences
 from nltk.tokenize.treebank import TreebankWordTokenizer
-
-from pytorch_pretrained_bert import convert_tf_checkpoint_to_pytorch
-from pytorch_pretrained_bert import BertTokenizer, BertConfig
-from pytorch_pretrained_bert.modeling import BertPreTrainedModel, BertModel
-from pytorch_pretrained_bert.optimization import BertAdam
-
-class MyBertClassifier(BertPreTrainedModel):
-
-    def __init__(self, config, num_aux_targets):
-        super(MyBertClassifier, self).__init__(config)
-
-        self.bert = BertModel(config)
-        self.dropout = nn.Dropout(0.2)
-        self.linear = nn.Linear(config.hidden_size, config.hidden_size)
-        self.linear_out = nn.Linear(config.hidden_size, 1)
-        self.linear_aux_out = nn.Linear(config.hidden_size, num_aux_targets)
-        self.apply(self.init_bert_weights)
-
-    def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None):
-
-        _, pooled_output = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
-        pooled_output = self.dropout(pooled_output)
-
-        h_conc_linear1  = F.relu(self.linear(pooled_output))
-        h_conc_linear1 = self.dropout(h_conc_linear1)
-
-        hidden = pooled_output + h_conc_linear1
-        result = self.linear_out(hidden)
-        aux_result = self.linear_aux_out(hidden)
-        out = torch.cat([result, aux_result], 1)
-
-        return out
+from pytorch_pretrained_bert import BertTokenizer
 
 def seed_everything(seed=1235):
 
@@ -91,42 +60,24 @@ def trim_tensors(tsrs):
 
     return tsrs
 
-def calculate_toxicity(test_data):
+def calculate_toxicity(model, test_data):
 
     batch_size = 1
     max_bert_length = 220
     pytorch_conversion = False
 
     seed_everything(1235)
+    device = torch.device('cpu')
     tqdm.pandas()
 
     bert_model_path = "./service/uncased_L-12_H-768_A-12/"
-    trained_model_path = "./service/bert_pytorch_model.bin"
-    bert_config = BertConfig(bert_model_path + 'bert_config.json')
-
-    if pytorch_conversion == True:
-        convert_tf_checkpoint_to_pytorch.convert_tf_checkpoint_to_pytorch(
-            bert_model_path + 'bert_model.ckpt',
-            bert_model_path + 'bert_config.json',
-            bert_model_path + 'pytorch_model.bin')
-
     base_tokenizer = BertTokenizer.from_pretrained(bert_model_path, cache_dir=None, do_lower_case=True)
     converted_text = convert_data(test_data, max_bert_length, base_tokenizer)
     bert_test_lengths = torch.from_numpy(np.array([len(x) for x in converted_text]))
     bert_test_set = torch.tensor(pad_sequences(converted_text, maxlen=max_bert_length, padding='post'), dtype=torch.long)
 
-    model = MyBertClassifier(bert_config, 6)
     bert_test_dataset = torch.utils.data.TensorDataset(bert_test_set)
     bert_test_loader = torch.utils.data.DataLoader(bert_test_dataset, batch_size=batch_size, shuffle=False)
-
-    device = torch.device('cpu')
-    model.load_state_dict(torch.load(trained_model_path, map_location=torch.device('cpu')))
-    model.to(device)
-
-    for param in model.parameters():
-        param.requires_grad = False
-
-    model.eval()
     tk2 = tqdm(enumerate(bert_test_loader), total=len(bert_test_loader), leave=False)
 
     output_preds = []
--- service/flask_service.py
+++ service/flask_service.py
@@ -4,15 +4,64 @@ warnings.filterwarnings('ignore')
 import json
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
+
+import torch
+from torch import nn
+from torch.utils import data
+import torch.nn.functional as F
+
 from service.bert_service import calculate_toxicity
+from pytorch_pretrained_bert.modeling import BertPreTrainedModel, BertModel
+from pytorch_pretrained_bert import BertTokenizer, BertConfig
 
 app = Flask(__name__)
 api = CORS(app, resources={r"/*": {"origins": "*"}})
 
+class MyBertClassifier(BertPreTrainedModel):
+
+    def __init__(self, config, num_aux_targets):
+        super(MyBertClassifier, self).__init__(config)
+
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(0.2)
+        self.linear = nn.Linear(config.hidden_size, config.hidden_size)
+        self.linear_out = nn.Linear(config.hidden_size, 1)
+        self.linear_aux_out = nn.Linear(config.hidden_size, num_aux_targets)
+        self.apply(self.init_bert_weights)
+
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None):
+
+        _, pooled_output = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
+        pooled_output = self.dropout(pooled_output)
+
+        h_conc_linear1  = F.relu(self.linear(pooled_output))
+        h_conc_linear1 = self.dropout(h_conc_linear1)
+
+        hidden = pooled_output + h_conc_linear1
+        result = self.linear_out(hidden)
+        aux_result = self.linear_aux_out(hidden)
+        out = torch.cat([result, aux_result], 1)
+
+        return out
+
+bert_model_path = "./service/uncased_L-12_H-768_A-12/"
+trained_model_path = "./service/bert_pytorch_model.bin"
+bert_config = BertConfig(bert_model_path + 'bert_config.json')
+
+device = torch.device('cpu')
+model = MyBertClassifier(bert_config, 6)
+model.load_state_dict(torch.load(trained_model_path, map_location=torch.device('cpu')))
+model.to(device)
+
+for param in model.parameters():
+    param.requires_grad = False
+
+model.eval()
+
 @app.route('/toxicity', methods=["POST"])
 def toxicity():
     toxic_string = [str(request.get_json()["inputText"])]
-    toxicity = str(calculate_toxicity(toxic_string)[0][0])
+    toxicity = str(calculate_toxicity(model, toxic_string)[0][0])
     output_toxicity = {
         'toxicityScore': toxicity
     }
@@ -23,4 +72,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
